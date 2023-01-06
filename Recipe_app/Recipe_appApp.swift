6eac47b626af5432b5ab104aefc99c8046fc1db7
imran <imran40031@gmail.com>
2023-01-06 23:59:10 +0600

    Initial Commit

---
+++ Recipe_app/Recipe_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_appApp.swift
+//  Recipe_app
+//
+//  Created by md imran on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
